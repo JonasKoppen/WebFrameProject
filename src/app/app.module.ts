2a5d88806f1a212e3b0e23309d80f485cedf36ec
Jonas Koppen DRWork <dreaddrone@gmail.com>
2017-12-14 09:25:39 +0100

    add the base project

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule,  NO_ERRORS_SCHEMA  } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MDBBootstrapModule.forRoot()
+  ],
+  schemas: [ NO_ERRORS_SCHEMA ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
