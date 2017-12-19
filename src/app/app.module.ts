8b57f0992d2d20d81a21bee15326b869d17649ef
JonasKoppen_DROS <dreaddrone@gmail.com>
2017-12-19 22:36:08 +0100

    added more

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule,  NO_ERRORS_SCHEMA  } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
+import {RouterModule} from '@angular/router';
+import { PageNotFoundComponent } from './pageNotFound/pageNotFound.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot([
+      {path : "", redirectTo: "home", pathMatch: "full"},
+      {path: "404", component: PageNotFoundComponent},
+      {path: "home", component : HomeComponent}
+    ], {useHash:true}),
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
