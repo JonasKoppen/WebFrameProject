cbd1fbc3c88c81033492c5232f9b26130cbe3d63
Jonas Koppen DRWork <dreaddrone@gmail.com>
2018-02-19 12:47:18 +0100

    server complete

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const port = 80;
 
 const server = express();
 
+server.use(express.static(path.join(__dirname, 'dist')));
+
 server.listen(port,hostname, () => {
     console.log(`Server is running at http://${hostname}:${port}/`);
 });
